525f7edc9fc7bc3825de131e636fd988981814ec
Derek Lieu <dl@dereklieu.com>
2016-11-23 08:25:13 -0800

    Attach link to home button in 404

--- app/assets/scripts/views/uhoh.js
+++ app/assets/scripts/views/uhoh.js
@@ -1,6 +1,7 @@
 'use strict';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 import PageFooter from '../components/page-footer';
 import PageHeader from '../components/page-header';
 
@@ -27,7 +28,7 @@ var UhOh = React.createClass({
         <div className='error-content'>
           <h1 className='heading--xxlarge heading--deco'>Page Not Found</h1>
           <p>We were not able to find the page you're looking for.</p>
-          <button className='button button--primary button--large'>Take Me Home</button>
+          <Link to='/' className='button button--primary button--large'>Take Me Home</Link>
         </div>
       </div>
       <PageFooter />
