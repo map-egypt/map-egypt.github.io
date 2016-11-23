95972b26900d3d0e83db6dad63787a29ed3d0d68
Anna Scalamogna <anna@developmentseed.org>
2016-11-23 16:41:37 -0500

    fix category placement and styling

--- app/assets/scripts/components/project-card.js
+++ app/assets/scripts/components/project-card.js
@@ -38,13 +38,6 @@ var ProjectCard = React.createClass({
         <article className={'card project--' + ontime ? 'ontime' : 'delayed'}>
           <div className='card__contents'>
             <header className='card__header'>
-              {project.categories.map((c, i) => {
-                return (
-                  <p key={i} className='card__subtitle'>
-                    <Link to={categoryLink(basepath, c)} className='link--secondary' href=''>{c}</Link>
-                  </p>
-                );
-              })}
               <h1 className='card__title heading--small'><Link to={path.resolve(basepath, 'projects', project.id)} className='link--deco' href=''>{project.name}</Link></h1>
 
               <ul className='card-cmplt'>
@@ -59,6 +52,15 @@ var ProjectCard = React.createClass({
                 <dd className='card-meta__value card-meta__value--location'>{project.location.map((loc) => loc.district.governorate).join(', ')}</dd>
               </dl>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et dui gravida, posuere diam id, congue augue. Pellentesque nec purus ex. Vestibulum ante.</p>
+              <div className='card__categories'>
+                {project.categories.map((c, i) => {
+                  return (
+                    <span key={i} className='card__subtitle'>
+                      <Link to={categoryLink(basepath, c)} className='link--secondary' href=''>{c},</Link>
+                    </span>
+                  );
+                })}
+              </div>
               <ul className='card-stats'>
                 <li>$50M <small>funding</small></li>
                 <li>20,000 <small>households</small></li>
