fc13ea607f9c880b5183e48b984ecbdd23d321ce
Derek Lieu <dl@dereklieu.com>
2016-11-23 08:21:37 -0800

    Include page header, footer in 404

--- app/assets/scripts/views/uhoh.js
+++ app/assets/scripts/views/uhoh.js
@@ -1,23 +1,46 @@
 'use strict';
 import React from 'react';
+import { connect } from 'react-redux';
+import PageFooter from '../components/page-footer';
+import PageHeader from '../components/page-header';
 
 var UhOh = React.createClass({
   displayName: 'UhOh',
 
+  propTypes: {
+    location: React.PropTypes.object,
+    api: React.PropTypes.object,
+    dispatch: React.PropTypes.func,
+    meta: React.PropTypes.object
+  },
+
   render: function () {
     return (
-    <div className='error'>
-      <div className='error-content'>
-        <h1 className='heading--xxlarge heading--deco'>Page Not Found</h1>
-        <p>We were not able to find the page you're looking for.</p>
-        <button className='button button--primary button--large'>Take Me Home</button>
+    <div>
+      <PageHeader
+        location={this.props.location}
+        dispatch={this.props.dispatch}
+        authenticated={this.props.api.authenticated}
+        lang={this.props.meta.lang || 'en'}
+      />
+      <div className='error'>
+        <div className='error-content'>
+          <h1 className='heading--xxlarge heading--deco'>Page Not Found</h1>
+          <p>We were not able to find the page you're looking for.</p>
+          <button className='button button--primary button--large'>Take Me Home</button>
+        </div>
       </div>
+      <PageFooter />
     </div>
     );
   }
 });
 
-// /////////////////////////////////////////////////////////////////// //
-// Connect functions
+function mapStateToProps (state) {
+  return {
+    api: state.api,
+    meta: state.meta
+  };
+}
 
-export default UhOh;
+module.exports = connect(mapStateToProps)(UhOh);
