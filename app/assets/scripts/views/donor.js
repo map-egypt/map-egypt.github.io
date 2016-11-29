fd7470422d21166d857fb024589e85c176033a3a
Marc Farra <marcfarra@gmail.com>
2016-11-29 15:23:16 -0500

    Add initial donor page data

--- app/assets/scripts/views/donor.js
+++ app/assets/scripts/views/donor.js
@@ -1,14 +1,78 @@
 'use strict';
 import React from 'react';
+import { connect } from 'react-redux';
+import { get } from 'object-path';
+import Share from '../components/share';
+import Map from '../components/map';
 
 var Donor = React.createClass({
   displayName: 'Donor',
 
+  propTypes: {
+    params: React.PropTypes.object,
+    location: React.PropTypes.object,
+    api: React.PropTypes.object,
+    meta: React.PropTypes.object
+  },
+
   render: function () {
+    const projects = get(this.props, 'api.projects', []);
+    if (projects.length === 0) {
+      return <div></div>; // TODO loading indicator
+    }
+
+    const donorName = this.props.params.name;
+
+    const donorProjects = projects.filter((project) => {
+      return get(project, 'budget', []).some((item) => {
+        return item.donor_name.toLowerCase() === donorName.toLowerCase();
+      });
+    });
+
+    const projectBudgets = donorProjects
+      .map((project) => project.budget)
+      .reduce((a, b) => a.concat(b), []);
+
+    // TODO change this to 2 amounts dispursed and remaining
+    const totalBudget = projectBudgets.reduce((currentValue, budget) => {
+      return budget.fund.amount + currentValue;
+    }, 0);
+
     return (
-      <div>
-        <h1>Donor</h1>
-      </div>
+      <section className='inpage'>
+        <header className='inpage__header'>
+          <div className='inner'>
+            <div className='inpage__headline'>
+              <div className='inpage__headline-actions'>
+                <ul className='inpage-stats'>
+                  <li><button className='button button--medium button--primary button--download'>Download</button></li>
+                  <li><Share path={this.props.location.pathname}/></li>
+                </ul>
+              </div>
+            </div>
+            <h1 className='inpage__title heading--deco heading--large'>{donorName}</h1>
+          </div>
+        </header>
+        <div className='inpage__body'>
+          <div className='inner'>
+            <section className='inpage__section inpage__section--overview'>
+
+              <h1 className='visually-hidden'>Project Overview</h1>
+              <div className='inpage__col--map'>
+                <Map />
+              </div>
+              <div className='inpage__col--content'>
+                <ul className='inpage-stats'>
+                  <li>{Math.floor(totalBudget / 1000000)}M <small>Total Funds</small></li>
+                </ul>
+              </div>
+
+              <div className='inpage__overview-chart'>
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
     );
   }
 });
@@ -16,4 +80,11 @@ var Donor = React.createClass({
 // /////////////////////////////////////////////////////////////////// //
 // Connect functions
 
-export default Donor;
+function mapStateToProps (state) {
+  return {
+    api: state.api,
+    meta: state.meta
+  };
+}
+
+module.exports = connect(mapStateToProps)(Donor);
