c46b87a2a9784583719f99642341d16c41804737
Derek Lieu <dl@dereklieu.com>
2016-12-01 16:15:13 -0500

    Use turf bbox, make map fit bounds

--- app/assets/scripts/components/map.js
+++ app/assets/scripts/components/map.js
@@ -1,18 +1,48 @@
 'use strict';
 import React from 'react';
 import window from 'global/window';
+import bbox from '@turf/bbox';
 
 const tileLayer = 'https://api.mapbox.com/styles/v1/map-egypt/civld9uy0000n2kmnd7lqs3ne/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwLWVneXB0IiwiYSI6ImNpdmxkMjl6bTA3c2YyeXBvNDJnZDlqZGMifQ.KQSizb18ILr6wri0cBcd2Q';
+
+const BOUNDS = [
+  [22.278144, 25.127830],
+  [31.118067, 33.719138]
+];
+
+function getLatLngBounds (bounds) {
+  let b = bbox(bounds);
+  let result = [[b[1], b[0]], [b[3], b[2]]];
+  return result;
+}
 
 const Map = React.createClass({
   displayName: 'Map',
 
   propTypes: {
-    api: React.PropTypes.object
+    location: React.PropTypes.object
+  },
+
+  fitMap: function (props) {
+    let bounds;
+    if (props.location) {
+      bounds = props.location.type
+        ? getLatLngBounds(props.location)
+        : props.location;
+    } else {
+      bounds = BOUNDS;
+    }
+    this.map.fitBounds(bounds);
   },
 
   componentWillUnmount: function () {
     this.map.remove();
+  },
+
+  componentWillReceiveProps: function (newProps) {
+    if (newProps.location && (!this.props.location || JSON.stringify(newProps.location) !== JSON.stringify(this.props.location))) {
+      this.fitMap(newProps);
+    }
   },
 
   mountMap: function (el) {
@@ -21,11 +51,8 @@ const Map = React.createClass({
         scrollWheelZoom: false
       });
       window.L.tileLayer(tileLayer).addTo(this.map);
-      this.map.fitBounds([
-        [22.278144, 25.127830],
-        [31.118067, 33.719138]
-      ]);
     }
+    this.fitMap(this.props);
   },
 
   render: function () {
