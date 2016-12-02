f55997e2755369d4070bb7f18608fa1d19d80f31
Marc Farra <marcfarra@gmail.com>
2016-12-01 19:03:20 -0500

    Add overview charts

--- app/assets/scripts/views/category.js
+++ app/assets/scripts/views/category.js
@@ -25,6 +25,46 @@ var Category = React.createClass({
       return <div></div>; // TODO loading indicator
     }
 
+    // Count number of projects per category
+    const justCategories = projects.map((project) => {
+      let budget = project.budget.reduce((cur, item) => cur + item.fund.amount, 0);
+      return get(project, 'categories', []).map((category) => {
+        return { category, budget };
+      });
+    }).reduce((a, b) => a.concat(b), []);
+
+    let numProjectsPerCategory = {};
+    let budgetPerCategory = {};
+    justCategories.forEach((categoryObj) => {
+      const {category, budget} = categoryObj;
+      if (category in numProjectsPerCategory) {
+        numProjectsPerCategory[category] += 1;
+      } else {
+        numProjectsPerCategory[category] = 1;
+      }
+
+      if (category in budgetPerCategory) {
+        budgetPerCategory[category] += budget;
+      } else {
+        budgetPerCategory[category] = budget;
+      }
+    });
+
+    const numProjectsChartData = Object.keys(numProjectsPerCategory).map((key) => {
+      return {
+        name: key.substring(0, 25) + '...',
+        value: numProjectsPerCategory[key]
+      };
+    }).sort((a, b) => b.value > a.value ? -1 : 1);
+
+    const budgetPerCategoryChartData = Object.keys(budgetPerCategory).map((key) => {
+      return {
+        name: key.substring(0, 25) + '...',
+        value: budgetPerCategory[key]
+      };
+    }).sort((a, b) => b.value > a.value ? -1 : 1);
+
+    // //////////////////////////////
     const categoryName = this.props.params.name;
     let categoryDisplayName;
 
@@ -54,7 +94,7 @@ var Category = React.createClass({
       name: i,
       value: ProjectCard.percentComplete(d.project)
     }));
-    
+
     const totalBudget = categoryBudgets.reduce((currentValue, budget) => {
       return budget.fund.amount + currentValue;
     }, 0);
@@ -87,6 +127,21 @@ var Category = React.createClass({
           <div className='inner'>
             <section className='inpage__section'>
               <h1 className='section__title heading--small'>Comparison</h1>
+              <div className='chart-content'>
+                <h3>Number of Projects per Category</h3>
+                <HorizontalBarChart
+                  data={numProjectsChartData}
+                  margin={{ left: 150, right: 20, top: 10, bottom: 50 }}
+                />
+              </div>
+              <div className='chart-content'>
+                <h3>Funding of Projects per Category</h3>
+                <HorizontalBarChart
+                  data={budgetPerCategoryChartData}
+                  margin={{ left: 150, right: 20, top: 10, bottom: 50 }}
+                  xFormat={shortTally}
+                />
+              </div>
             </section>
             <section className='inpage__section'>
               <h1 className='section__title heading--small'>Projects</h1>
