8051d1c5c70d0649ab34aa2c1d1e72af56110c58
Derek Lieu <dl@dereklieu.com>
2016-12-03 13:10:32 -0500

    Remove token when it's expired

--- app/assets/scripts/utils/auth.js
+++ app/assets/scripts/utils/auth.js
@@ -38,8 +38,14 @@ function isTokenExpired (token) {
   return !(date.valueOf() > (new Date().valueOf() + (offsetSeconds * 1000)));
 }
 
-let token = store.get('id_token');
-let isAuthenticated = !!token && !isTokenExpired(token);
+const token = store.get('id_token');
+const isExpired = token && isTokenExpired(token);
+let isAuthenticated = !!token && !isExpired;
+
+// remove token if it's expired
+if (token && isExpired) {
+  store.remove('id_token');
+}
 
 const dispatches = [];
 auth.on('authenticated', function (authResult) {
