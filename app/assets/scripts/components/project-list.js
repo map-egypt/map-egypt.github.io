da8798871388d550b6fad7eabb710fdf8793e015
Derek Lieu <dl@dereklieu.com>
2017-01-10 16:12:42 -0500

    Fix #18 make alphabetical the default sort for projects

--- app/assets/scripts/components/project-list.js
+++ app/assets/scripts/components/project-list.js
@@ -10,8 +10,29 @@ const ProjectList = React.createClass({
     meta: React.PropTypes.object
   },
 
+  getInitialState: function () {
+    return {
+      showSort: false,
+      sortAccessor: (d) => d.name
+    };
+  },
+
+  toggleSort: function () {
+    this.setState({showSort: !this.state.showSort});
+  },
+
+  setSortAccessor: function (prop) {
+    this.setState({
+      sortAccessor: prop,
+      showSort: false
+    });
+  },
+
   render: function () {
     const {projects, meta} = this.props;
+    const {sortAccessor} = this.state;
+    const data = projects.slice().sort((a, b) => sortAccessor(a) < sortAccessor(b) ? -1 : 1);
+
     return <div className='inpage__body'>
       <div className='inner'>
         <section className='inpage__section project-list'>
@@ -19,12 +40,22 @@ const ProjectList = React.createClass({
             <h1 className='section__title'>Projects</h1>
             <div className='sort'>
               <label className='heading--label'>Sort By:</label>
-              <button className='button button--medium button--secondary drop__toggle--caret'>Category</button>
+              <span className='dropdown__container'>
+                <button className='button button--medium button--secondary drop__toggle--caret'
+                onClick={this.toggleSort}>Alphabetical</button>
+                {this.state.showSort &&
+                  <ul className='drop__menu drop--align-left button--secondary'>
+                    <li className='drop__menu-item'
+                      onClick={() => this.setSortAccessor((d) => d.name)}
+                      >Alphabetical</li>
+                  </ul>
+                }
+              </span>
             </div>
           </div>
           <div className='section__content'>
             <ul className='projects-list'>
-              {projects.map((p) => {
+              {data.map((p) => {
                 return (
                   <li key={p.id}
                     className='projects-list__card'>
