2f6d6b22b692b08e97f416c8aba70ee41db92c2a
Anna Scalamogna <anna@developmentseed.org>
2017-01-19 15:37:25 -0500

    increase logo size, update text size and color, add button to homepage

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -156,6 +156,7 @@ var Home = React.createClass({
             <ul className='section__footer'>
               <li><Link to={'/' + lang + '/projects_sds'} type='button' className='button button--primary button--large'>{t.sds_button}</Link></li>
               <li><Link to={'/' + lang + '/projects_sdg'} type='button' className='button button--primary button--large'>{t.sdg_button}</Link></li>
+              <li><Link to={'/' + lang + '/projects_other'} type='button' className='button button--primary button--large'>{t.other_button}</Link></li>
             </ul>
           </section>
         </div>
