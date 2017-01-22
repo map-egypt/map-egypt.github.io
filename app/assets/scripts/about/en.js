69e93d055e33bde15e27ae3d1a24710fd8937ec0
hagareldidi <h.eldidi@cgiar.org>
2017-01-22 13:02:13 +0200

    Update en.js

--- app/assets/scripts/about/en.js
+++ app/assets/scripts/about/en.js
@@ -3,8 +3,8 @@ import React from 'react';
 const content = (
   <div>
     <p>MAP Egypt is a monitoring, mapping and charting tool that aims to help keep track of all the agricultural development projects in Egypt, their targets and objectives. The ultimate goal of the tool is to improve agricultural project planning, coordination and effectiveness. MAP Egypt can help decision makers, governments, donors and researchers to allocate funds, build synergies and evaluate future proposals, by visualizing where agricultural development is most needed or least targeted. 
-Users can visualize and monitor the status and progress of agricultural projects categorized by sector, location, donor or common objectives, as well as track key performance indicators related to the project. In order to contextualize projects and evaluate which governorates /districts require further assistance, users can also quickly map and chart a wide-range of indicators on food and nutrition security, poverty and development across Egypt, with a focus on monitoring progress towards the Sustainable Development Goals (SDGs) and Egypt’s Sustainable Development Strategy (SDS) - on the national and subnational level. 
-</p>
+Users can visualize and monitor the status and progress of agricultural projects categorized by sector, location, donor or common objectives, as well as track key performance indicators related to the project. In order to contextualize projects and evaluate which governorates /districts require further assistance, users can also quickly map and chart a wide-range of indicators on food and nutrition security, poverty and development across Egypt, with a focus on monitoring progress towards the Sustainable Development Goals (SDGs) and Egypt’s Sustainable Development Strategy (SDS) - on the national and subnational level.</p>
+    <p className='small'>The information provided on this website is not official U.S. Government information and does not represent the views or positions of the U.S. Agency for International Development or the U.S. Government. </p>
   </div>
 );
 module.exports = content;
