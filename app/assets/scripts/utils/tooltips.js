d769c7e1ff4760eba13f1103908f5ddbdaf1e045
Taher ElShoker <taher.elshoker@cartologic.com>
2020-08-10 15:27:22 +0200

    fix Indicator Info Localization

--- app/assets/scripts/utils/tooltips.js
+++ app/assets/scripts/utils/tooltips.js
@@ -1,7 +1,8 @@
 import { get } from 'object-path';
 export function indicatorTooltipContent (indicator, lang) {
   const t = get(window.t, [lang, 'map_labels'], {});
+  const defaultDescriptionLabel = t.description_default_value;
   const indicatorDescription = lang === 'en' ? indicator.description : indicator.description_ar;
-  return `${(indicator.description) !== null ? `<span class="tooltip__description">${t.description_label}: ${indicatorDescription}</span>` : ''}` +
+  return `<span class="tooltip__description">${t.description_label} : &nbsp; ${indicatorDescription !== null ? indicatorDescription : defaultDescriptionLabel}</span>` +
     `${(indicator.sources) ? `<span class="tooltip__sources">Sources: ${indicator.sources.join(', ')}</span>` : ''}`;
 }
